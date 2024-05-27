4a4013f27676207d0c12cf71fea1cd9d46ff65ad
Louis Beullens <louis@beullens.com>
2024-05-27 17:14:05 +0200

    addded basename to browserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/mol-poll">
         <Provider store={store}>
           <Routes>
             <Route path="/admin/*" element={<AdminApp />} />
